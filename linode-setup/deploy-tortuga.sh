d1f9d4eaabd89b9fdda8f55317fc301ca7cc93f1
Pieter Michels <pieter@noort.be>
2011-09-13 21:37:14 +0200

    no deploy script

--- linode-setup/deploy-tortuga.sh
+++
@@ -1,5 +0,0 @@
-ssh root@tortuga 'bash -s' < install.sh
-ssh root@tortuga 'bash -s' < install.sh
-
-# bash <( curl http://foo.com/script.sh ) arguments
-# curl http://foo.com/script.sh | bash -s arg1 arg2
