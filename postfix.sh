c7e22ec823b07cbc08d55e8b950055eff8cb85c4
Pieter Michels <pieter@noort.be>
2011-09-21 08:41:29 +0200

    postfix test code

--- postfix.sh
+++ postfix.sh
@@ -20,6 +20,40 @@ _postfix_loopback_only() {
   sudo touch /tmp/restart-postfix
 }
 
+_testing() {
+  _print "How to test:
+$ telnet localhost 25
+
+Trying 127.0.0.1...
+Connected to localhost.localdomain.
+Escape character is '^]'.
+220 tortuga.local ESMTP Postfix (Ubuntu)
+
+  HELO tortuga
+250 tortuga.local
+
+  MAIL FROM:<pieter@tortuga>
+250 2.1.0 Ok
+
+  RCPT TO:<pieter@noort.be>
+250 2.1.5 Ok
+
+  DATA
+354 End data with <CR><LF>.<CR><LF>
+  Subject: test message
+  This is the body!
+  .
+250 2.0.0 Ok: queued as 8E2B854D5A
+
+  QUIT
+221 2.0.0 Bye
+
+Connection closed by foreign host.
+"
+}
+
 ###############################################################################
 
 _postfix_loopback_only
+
+_testing
