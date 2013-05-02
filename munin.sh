c47f124f87d975511c5db094433b2e2ccb7258eb
Pieter Michels <pieter@noort.be>
2013-05-02 13:40:50 +0200

    tweak

--- munin.sh
+++ munin.sh
@@ -72,15 +72,6 @@ _munin() {
   sudo perl -pi -e "s/#html_strategy cgi/html_strategy cgi/" "/etc/munin/munin.conf"
   sudo perl -pi -e "s/#graph_strategy cgi/graph_strategy cgi/" "/etc/munin/munin.conf"
 
-#   _print "Add munin-node-config to /etc/munin/munin-node.conf"
-#
-#   munin_node_config="
-# allow ^127\.0\.0\.1$\n
-# host 127.0.0.1\n
-# "
-#
-#   echo -e $munin_node_config | sudo tee -a /etc/munin/munin-node.conf > /dev/null
-
   # Find and replace
   _print "Add nginx-stub-status to "$nginx_dir"/conf/nginx.conf"
 
@@ -126,18 +117,25 @@ server {
   # pass the PHP scripts to FastCGI server
   location ~ \.php$ {
     root           html;
-    fastcgi_pass   127.0.0.1:9000;
-    fastcgi_index  index.php;
-    fastcgi_param  SCRIPT_FILENAME  /var/cache/munin/www\$fastcgi_script_name;
-    include        fastcgi_params;
+    fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    fastcgi_param  SCRIPT_FILENAME  /var/cache/munin/www$fastcgi_script_name;
+    include fastcgi_params;
   }
 
   location / {
-    # auth_basic            "Restricted";
-    # auth_basic_user_file  /srv/conf/htpasswd;
+    auth_basic            "Restricted";
+    auth_basic_user_file  /srv/www/htpasswd;
 
-    root          /var/cache/munin/www/;
-    index         index.html index.php;
+    fastcgi_split_path_info ^(/)(.*);
+    fastcgi_param PATH_INFO \$fastcgi_path_info;
+    fastcgi_pass  unix:/var/run/munin/fastcgi-html.sock;
+    include       fastcgi_params;
+  }
+
+  location /static/ {
+    alias         /etc/munin/static/;
   }
 
   location ^~ /cgi-bin/munin-cgi-graph/ {
@@ -147,17 +145,6 @@ server {
     fastcgi_pass  unix:/var/run/munin/fastcgi-graph.sock;
     include       fastcgi_params;
   }
-
-  location /munin/static/ {
-    alias         /etc/munin/static/;
-  }
-
-  location /munin/ {
-    fastcgi_split_path_info ^(/munin)(.*);
-    fastcgi_param PATH_INFO \$fastcgi_path_info;
-    fastcgi_pass  unix:/var/run/munin/fastcgi-html.sock;
-    include       fastcgi_params;
-  }
 }
 EOS
 
@@ -165,39 +152,11 @@ EOS
 
   _print "Restart nginx"
 
-  sudo /etc/init.d/nginx reload
-}
-
-_munin_ngxin_plugins() {
-  _print_h2 "Add munin plugins for requests, status and memory"
-
-  cd /usr/share/munin/plugins
-  sudo wget -O nginx_request https://raw.github.com/munin-monitoring/contrib/master/plugins/nginx/nginx_request
-  sudo wget -O nginx_status https://raw.github.com/munin-monitoring/contrib/master/plugins/nginx/nginx_status
-  sudo wget -O nginx_memory https://raw.github.com/munin-monitoring/contrib/master/plugins/nginx/nginx_memory
-
-  sudo chmod +x nginx_request
-  sudo chmod +x nginx_status
-  sudo chmod +x nginx_memory
-
-  sudo ln -s /usr/share/munin/plugins/nginx_request /etc/munin/plugins/nginx_request
-  sudo ln -s /usr/share/munin/plugins/nginx_status /etc/munin/plugins/nginx_status
-  sudo ln -s /usr/share/munin/plugins/nginx_memory /etc/munin/plugins/nginx_memory
-
-  _print "Edit /etc/munin/plugin-conf.d/munin-node"
-
-  munin_env="\n
-[nginx*]\n
-env.url http://localhost/nginx_status\n
-"
-
-  echo -e $munin_env | sudo tee -a /etc/munin/plugin-conf.d/munin-node > /dev/null
-
+  sudo service nginx reload
 }
 
 ###############################################################################
 
 _munin
-# _munin_ngxin_plugins
 
 _note_installation "munin"
