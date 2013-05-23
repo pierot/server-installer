76017976b2cb92279801181b39e3385c993d7dda
Pieter Michels <pieter@noort.be>
2013-05-23 14:23:19 +0200

    no update or upgrade

--- base.sh
+++ base.sh
@@ -93,8 +93,8 @@ _hostname() {
 _system_installs() {
 	_print_h2 "System wide installs"
 
-  sudo apt-get -qq update
-  sudo apt-get -qq upgrade
+  # sudo apt-get -qq update
+  # sudo apt-get -qq upgrade
 
   # _system_installs_install 'aptitude'
 
